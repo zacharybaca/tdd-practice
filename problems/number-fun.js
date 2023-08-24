1ea1e1acf48b9bb708475b3011b29f2cad342875
Zachary Baca <zachary.baca@msn.com>
2023-08-24 09:22:12 -0500

    changed reciprocal function

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -5,14 +5,18 @@ function returnsThree() {
 
 function reciprocal(n) {
   // Your code here
-  if (Number.isInteger(n)) {
+  try {
+    if (Number.isInteger(n)) {
     return 1/n;
-  } else if (Number.isNaN(n)) {
+  } else if (typeof n !== "number") {
     throw new TypeError("Input is not an Integer");
   } else {
     let decNum = 1/n;
     return decNum.toFixed(3);
   }
+ } catch(e) {
+     console.log(e.message);
+ }
 }
 
 module.exports = {
