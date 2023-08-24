81492f42b0e71d43217637eaa84e497700edc8c1
Zachary Baca <zachary.baca@msn.com>
2023-08-24 07:25:32 -0500

    created test for reciprocal

--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -7,5 +7,11 @@ const { reciprocal } = require('../problems/number-fun');
 describe('returnsThree function', function() {
     it('returns the number three', function() {
         expect(returnsThree()).to.equal(3);
-    })
-})
+    });
+});
+
+describe('reciprocal function', function() {
+    it('takes in a number, and returns the reciprocal of that number', function() {
+        expect(() => reciprocal(num)).to.equal(1/num);
+    });
+});
