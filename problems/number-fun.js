7d8a327c524d902eae42e2cf855034ffe07ec64e
Zachary Baca <zachary.baca@msn.com>
2023-08-24 08:44:00 -0500

    implemented additonal tests

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -5,6 +5,14 @@ function returnsThree() {
 
 function reciprocal(n) {
   // Your code here
+  if (Number.isInteger(n)) {
+    return 1/n;
+  } else if (Number.isNaN(n)) {
+    throw new TypeError("Input is not an Integer");
+  } else {
+    let decNum = 1/n;
+    return decNum.toFixed(3);
+  }
 }
 
 module.exports = {
