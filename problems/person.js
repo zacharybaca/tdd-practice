7c304a2e27841dcd6d0f66d73f310896827e921e
zacharybaca <zachary.baca@msn.com>
2023-08-24 19:05:18 -0500

    completed switchVisit tests

--- problems/person.js
+++ problems/person.js
@@ -1,5 +1,25 @@
 class Person {
-  // Your code here
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    return `Hello ${this.name}`
+  }
+
+  visit(otherPerson) {
+    return `${this.name} visited ${otherPerson.name}`;
+  }
+
+  switchVisit(otherPerson) {
+    return otherPerson.visit(this)
+  }
 }
+
+const bob = new Person('bob', 32);
+const bobby = new Person('bobby', 31);
+bob.visit(bobby);
+bob.switchVisit(bobby);
 
 module.exports = Person;
--- test/person-spec.js
+++ test/person-spec.js
@@ -1,1 +1,29 @@
 // Your code here
+const { expect } = require('chai');
+const Person = require('../problems/person');
+
+describe('Person class', function() {
+    let person;
+    let personTwo;
+    beforeEach(() => {
+        person = new Person('bob', 31);
+        personTwo = new Person('bobby', 32);
+    });
+    // it('will throw an error if the name parameter is not a string', function() {
+    //     const nameFunc = typeof person.name !== 'string';
+    //     expect(nameFunc).to.throw(TypeError('Name needs to be a string'));
+    // });
+    it('will take in a name and age property', function() {
+        expect(person).to.have.property('name');
+        expect(person).to.have.property('age');
+    });
+    it('sayHello method should return a string of name and greeting message', function() {
+        expect(person.sayHello()).to.equal(`Hello ${person.name}`);
+    });
+    it('should return a string of instance one visited instance two', function() {
+        expect(person.visit(personTwo)).to.equal(`${person.name} visited ${personTwo.name}`);
+    });
+    it('should invoke visit method inside of switchVisit method', function() {
+        expect(personTwo.switchVisit(person)).to.be.equal(person.visit(personTwo))
+    })
+});
